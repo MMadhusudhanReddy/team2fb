3a68bf9019c0318b241226239071d4de91900f72
Madhusudhan Reddy <madhusudhanreddy@Madhusudhans-MacBook-Pro.local>
2020-02-01 10:45:37 +0530

    commit21

--- src/views/commenttry.vue
+++ src/views/commenttry.vue
@@ -7,17 +7,41 @@
     Comment
   </button>
   <!-- The Modal -->
-  <div class="modal fade" id="myModal">
-    <div class="modal-dialog modal-sm">
+  <div class="modal fade" id="myModal" >
+    <div class="modal-dialog modal-sm" style="width:50%;">
       <div class="modal-content">
         <!-- Modal body -->
-        <div class="modal-body" v-for="(items,index) in list" v-bind:key="index">
-           <div class="one">{{items.comment}}</div>
-        </div>
+
+
+        <div v-for="(comment,index) in comments" :key="index" class="order">
+              <div class="parentcomment">
+                  <div class="comment">
+                    parentcommentid:  {{comment.parentid}}
+                  </div>
+                  <div class="comment">
+                    parenttext: {{comment.parenttext}}
+                  </div>
+                </div>
+                <!-- <hr style="background: 1px solid black; padding:0%;margin:0%;"> -->
+                <div class="orderlist">
+                <div v-for="(childcomment,commentindex) in comment.childcomments" :key="commentindex" class="item">
+                  <div class="childcomment">
+                      <div class="commentchild">
+                        childcommentid: {{childcomment.commentid}}
+                      </div>
+                      <div class="commentchild">
+                        commenttext:{{childcomment.commenttext}} 
+                      </div>
+                  </div>
+                </div> 
+                </div>
+            </div>
+
+
         <!-- Modal footer -->
         <div class="modal-footer">
-          <input type="text" placeholder="start typing.." id="text"><br><br>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="add">send</button>
+          <input type="text" placeholder="start typing.." id="text" style="width:70%;"><br><br>
+          <button type="button" class="btn btn-secondary" style="width:20%;" data-dismiss="modal" @click="add">send</button>
         </div>
       </div>
     </div>
@@ -29,19 +53,38 @@
 export default {
   data(){
     return{
-      list:[
-        {
-          comment:'',
-        },
-        {
-          comment:'',
-        },
-        {
-          comment:'',
-        },
-        {
-          comment:'',
-        },
+      comments:[
+          {
+              parentid:1,
+              parenttext:"hello world",
+              childcomments:[
+                  {
+                      commentid:11,
+                      commenttext:"hello mini world"
+                  },
+                  {
+                      commentid:12,
+                      commenttext:"this is fun"
+                  }
+              ]
+          },
+          {
+              parentid:2,
+              parenttext:"Welcome to fb!!!",
+              childcomments:[
+                  {
+                      commentid:21,
+                      commenttext:"childcomment 21"
+
+                  },
+                  {
+                      commentid:22,
+                      commenttext:"childcomment 22"
+
+                  }
+
+              ]
+          }
       ]
     }
   },
@@ -50,7 +93,7 @@ export default {
 <style scoped>
 .myModal{
   margin-top:20%;
-  width:500px;
+  width:60%;
   height:200px;
 }
 .one{
@@ -65,4 +108,16 @@ export default {
   border:1px solid black;
   float:right;
 }
+.comment{
+    margin: 4%;
+    border: 1px solid;
+    width: 60%;
+}
+.commentchild{
+    margin: 4%;
+    border: 1px solid;
+    float:right;
+
+}
+
 </style>
